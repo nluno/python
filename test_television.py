1ffd93be66cd66484d06e2d0119e90007a02ec51
Niko Luebbert <nluebbert@unomaha.edu>
2023-11-19 13:54:21 -0600

    Rewrote unit tests to use __str__().

--- test_television.py
+++ test_television.py
@@ -2,8 +2,7 @@
 import unittest
 from television import *
 
-#In order to not defeat the purpose of data hiding, I implemented a getter function I could use for unit testing.
-#I could have used the __str__ function to check the program state, but I thought I could create more readable unit tests by using a getter function instead of inferring Television's internal state from str.
+#I have rewritten my unit tests to use the __str__ function instead of a getter.
 
 class myTestCode(unittest.TestCase):
     def setUp(self):
@@ -15,121 +14,122 @@ class myTestCode(unittest.TestCase):
 
     def test_init(self):
         t1 = Television()
-        self.assertEqual(t1.for_testing_purposes_getter("status"), False)
-        self.assertEqual(t1.for_testing_purposes_getter("muted"), False)
-        self.assertEqual(t1.for_testing_purposes_getter("volume"), Television.MIN_VOLUME)
-        self.assertEqual(t1.for_testing_purposes_getter("channel"), Television.MIN_CHANNEL)
-
-        
+        self.assertEqual(t1.__str__(), "Power = False, Channel = 0, Volume = 0")
+                         
+      
     def test_power(self):
         t1 = Television()
-        self.assertEqual(t1.for_testing_purposes_getter("status"), False)
+
+        self.assertEqual(t1.__str__(), "Power = False, Channel = 0, Volume = 0")
         t1.power()
-        self.assertEqual(t1.for_testing_purposes_getter("status"), True)
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 0")
         t1.power()
-        self.assertEqual(t1.for_testing_purposes_getter("status"), False)
+        self.assertEqual(t1.__str__(), "Power = False, Channel = 0, Volume = 0")
         
-        
+
     def test_mute(self):
         t1 = Television()
         t1.power()
-        #Does mute() toggle the mute varible?
-        self.assertEqual(t1.for_testing_purposes_getter("muted"), False)
-        t1.mute()
-        self.assertEqual(t1.for_testing_purposes_getter("muted"), True)
-        t1.mute()
-        self.assertEqual(t1.for_testing_purposes_getter("muted"), False)
-
-        #Does changing the volume unmute?
-        t1.mute()
-        self.assertEqual(t1.for_testing_purposes_getter("muted"), True)
+        #the tv is on, the volume is increased once, and then the tv is muted:
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 0")
         t1.volume_up()
-        self.assertEqual(t1.for_testing_purposes_getter("muted"), False)
-
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 1")
         t1.mute()
-        self.assertEqual(t1.for_testing_purposes_getter("muted"), True)
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 0")
+        #The tv is on and unmuted:
+        t1.volume_up()
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 2")
+        #tv is off and muted:
+        t1.mute()
+        t1.power()
+        self.assertEqual(t1.__str__(), "Power = False, Channel = 0, Volume = 0")
+        #tv is off and unmuted:
+        t1.power()
         t1.volume_down()
-        self.assertEqual(t1.for_testing_purposes_getter("muted"), False)
-
+        t1.power()
+        self.assertEqual(t1.__str__(), "Power = False, Channel = 0, Volume = 1")
 
     def test_channel_up(self):
-        #Does it not turn up when it is off?
+        #tv is off and channel tries to increase:
         t1 = Television()
         t1.channel_up()
-        self.assertEqual(t1._Television__channel, Television.MIN_CHANNEL)
-        #Will it turn up when it is on?
+        self.assertEqual(t1.__str__(), "Power = False, Channel = 0, Volume = 0")
+        #tv is on and the channel increases:
         t1.power()
         t1.channel_up()
-        self.assertEqual(t1.for_testing_purposes_getter("channel"), (Television.MIN_CHANNEL + 1))
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 1, Volume = 0")
+        #increasing past the maximum channel:
         t1.channel_up()
-        self.assertEqual(t1.for_testing_purposes_getter("channel"), (Television.MIN_CHANNEL + 2))
         t1.channel_up()
-        self.assertEqual(t1.for_testing_purposes_getter("channel"), (Television.MIN_CHANNEL + 3))
-        #Did it sucessfully wrap around to the minimum channel?
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 3, Volume = 0")
         t1.channel_up()
-        self.assertEqual(t1.for_testing_purposes_getter("channel"), Television.MIN_CHANNEL)
-       
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 0")
 
+        
+        
     def test_channel_down(self):
-    #Does it not go down when it is off?
+        #Does it not go down when it is off?
         t1 = Television()
         t1.channel_down()
-        self.assertEqual(t1.for_testing_purposes_getter("channel"), Television.MIN_CHANNEL)
-        #Will it go down when it is on?
+        self.assertEqual(t1.__str__(), "Power = False, Channel = 0, Volume = 0")
+        #Does it decrease and wrap around when it is on?
         t1.power()
         t1.channel_down()
-        self.assertEqual(t1.for_testing_purposes_getter("channel"), (Television.MAX_CHANNEL))
-        t1.channel_down()
-        self.assertEqual(t1.for_testing_purposes_getter("channel"), (Television.MAX_CHANNEL - 1))
-        t1.channel_down()
-        self.assertEqual(t1.for_testing_purposes_getter("channel"), (Television.MAX_CHANNEL - 2))
-        #Did it sucessfully wrap around to the maximum channel?
-        t1.channel_down()
-        self.assertEqual(t1.for_testing_purposes_getter("channel"), Television.MIN_CHANNEL) #same as MAX_CHANNEL - 3
-        self.assertEqual(t1.for_testing_purposes_getter("channel"), Television.MAX_CHANNEL - 3)
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 3, Volume = 0")
+    
+
 
     def test_volume_up(self):
-    #Does it not turn up when it is off?
+        #Does it not turn up when it is off?
         t1 = Television()
         t1.volume_up()
-        self.assertEqual(t1.for_testing_purposes_getter("muted"), Television.MIN_VOLUME)
-        #Will it turn up when it is on?
+        self.assertEqual(t1.__str__(), "Power = False, Channel = 0, Volume = 0")
+        #Does it turn up when it is on?
         t1.power()
         t1.volume_up()
-        self.assertEqual(t1.for_testing_purposes_getter("volume"), (Television.MIN_VOLUME + 1))
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 1")
+        #What about when it is on, muted, and the volume is increased?
+        t1.mute()
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 0")
         t1.volume_up()
-        self.assertEqual(t1.for_testing_purposes_getter("volume"), (Television.MIN_VOLUME + 2))
-        #Does it do nothing if you try to go over the maximum volume?
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 2")
+        #What if you try to go over the maximum volume?
         t1.volume_up()
-        self.assertEqual(t1.for_testing_purposes_getter("volume"), (Television.MAX_VOLUME))
         t1.volume_up()
-        self.assertEqual(t1.for_testing_purposes_getter("volume"), (Television.MAX_VOLUME))
+        t1.volume_up()
+        t1.volume_up()
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 2")
         
+
     def test_volume_down(self):
-    #Does it not turn down when it is off?
+        #The tv is off and the volume is decreased:
         t1 = Television()
+        t1.volume_down()
+        self.assertEqual(t1.__str__(), "Power = False, Channel = 0, Volume = 0")
         t1.power()
+        #Increasing volume to maximum to prepare for test:
         t1.volume_up()
         t1.volume_up()
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 2")
+        #When the tv is on and the volume is decreased:
+        t1.volume_down()
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 1")
+        t1.volume_down()
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 0")
+        #Increasing volume to maximum to prepare for another test:
         t1.volume_up()
-        t1.power()
+        t1.volume_up()
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 2")
+        t1.mute()
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 0")
         t1.volume_down()
-        self.assertEqual(t1.for_testing_purposes_getter("volume"), Television.MAX_VOLUME)
-        #Will it turn down when it is on?
-        t1.power()
-        t1.volume_down()
-        self.assertEqual(t1.for_testing_purposes_getter("volume"), (Television.MAX_VOLUME -1))
-        t1.volume_down()
-        self.assertEqual(t1.for_testing_purposes_getter("volume"), (Television.MAX_VOLUME -2))
-        #Does it do nothing if you try to go below the minimum volume?
-        t1.volume_down()
-        self.assertEqual(t1.for_testing_purposes_getter("volume"), (Television.MIN_VOLUME))
-        t1.volume_down()
-        self.assertEqual(t1.for_testing_purposes_getter("volume"), (Television.MIN_VOLUME))
+        self.assertEqual(t1.__str__(), "Power = True, Channel = 0, Volume = 1")
+                
 '''
     def test_str(self):
         t1 = Television()
         t1.power()
+        
 '''
 
 
